7815898c5b8b32652ebe8fbc8a1327505d02f8db
Thais Pulliam <thaispulliam@gmail.com>
2015-11-04 14:06:44 -0500

    command+action+quote

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+// $('body').keypress(function() {
+// 	if (event.keyCode == 13) {
+// 		console.log( "Handler for .keypress() called." );
+//   		$('#log').append('<li>').append($('#prompt').val());
+//   	}
+// });
 jQuery(document).ready(function( ){
+
+var $command = $('.command');
+var $action = $('.action');
+var $quote = $('.quote');
 
 $('body').keypress(function() {
 	if (event.keyCode == 13) {
-		console.log( "Handler for .keypress() called." );
-  		$('#log').append('<li>').append($('#prompt').val());
-  	}
+		$command.text('Command: ' + $('#prompt').val());  
+		$action.text('Action: ' + $('#prompt').val());  
+		$quote.text('Quote: ' + $('#prompt').val());  	
+	}
 });
+
+
+
+
+
+
+
+
+
 
 });
