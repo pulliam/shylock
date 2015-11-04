21e708529ce393f48698731fc565b9f529eea282
Thais Pulliam <thaispulliam@gmail.com>
2015-11-04 12:14:53 -0500

    first mistake correction, still buggs

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+jQuery(document).ready(function( ){
+	
 var $input = $('#input');
 var $chat = $('#prompt');
 
 var newText = $input.val();
 
 var letsAppend = function(){
-$chat.append('li').append(newText);
+$chat.append('<li>').append(newText);
 }
 
 document.addEventListener('keypress', letsAppend);
+
+});
