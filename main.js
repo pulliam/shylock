c10de6dc882501df90530ea88c8fa299b697db02
Thais Pulliam <thaispulliam@gmail.com>
2015-11-04 12:07:30 -0500

    Initial Code, Asking for Help

---
+++ main.js
@@ -0,0 +1,10 @@
+var $input = $('#input');
+var $chat = $('#prompt');
+
+var newText = $input.val();
+
+var letsAppend = function(){
+$chat.append('li').append(newText);
+}
+
+document.addEventListener('keypress', letsAppend);
