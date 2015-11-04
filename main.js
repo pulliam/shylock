1076e7793717e6e48408b1511eda4e7fe554ee5e
Thais Pulliam <thaispulliam@gmail.com>
2015-11-04 15:50:47 -0500

    purseValue is not defined?

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 jQuery(document).ready(function(){
 // .match(/\d+/)
+var allLoans = [];
+var purseValue = 10000;
 
 $('#prompt').keypress(function() {
-	var purseValue = 10000;
 	var whatMyUserTyped = $('#prompt').val();
 	if (event.keyCode == 13) {
 		if (whatMyUserTyped === 'purse'){
 			$('#log li:last').text('ShylockBot pulls out his purse containing '+ purseValue+ ' ducats');	
 		} else {
+			this.subtractValue = subtractValue;
 			var subtractValue = parseInt(whatMyUserTyped.match(/\d+/));
-			purseValue = (purseValue - subtractValue); //This Part is getting stuck on the first defined value.
-			$('#log li:last').text('ShylockBot gives SOMEONE '+ subtractValue+ ' ducats');
+			purseValue = (purseValue - subtractValue); 
+			$('#log li:last').text('ShylockBot gives SOMEONE '+ subtractValue + ' ducats');
+			allLoans.push(subtractValue);
 		}
 	}
 });
